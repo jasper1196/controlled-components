034a7a2e4893630ea63527d21e90209d2cdb41a8
jasper1196 <jasper1196@outlook.com>
2021-10-14 23:13:45 +0200

    Figured out how to make it work with change handler for multiple events.

--- src/App.js
+++ src/App.js
@@ -2,63 +2,88 @@ import React from 'react';
 import './App.css';
 
 function App() {
-    const [nameValue, setNameValue] = React.useState("");
-    const [ageValue, setAgeValue] = React.useState(0);
-    const [remarkValue, setRemarkValue] = React.useState("");
     const [newsletterChecked, setNewsletterChecked] = React.useState(false);
 
-  return (
+    const [state, setState] = React.useState({
+        fullName: "",
+        age: "",
+        review: ""
+    });
+
+    function handleChange(e) {
+        const value = e.target.value;
+        setState({
+            ...state,
+            [e.target.name]: value
+        });
+    }
+
+    function handleSubmit(e) {
+        console.log(state.fullName);
+        console.log(state.age);
+        console.log(state.review);
+        console.log(newsletterChecked);
+        e.preventDefault();
+    }
+
+    function checkValues() {
+        return !state.fullName || state.age <= 0 || !state.review;
+    }
+
+    return (
     <div className="form-div">
-      <form>
-          <fieldset className="data-set">
-              <legend>Gegevens</legend>
-              <label htmlFor="name-input">Naam:
-                  <input
+        <form onSubmit={(e) => handleSubmit(e)}>
+            <fieldset className="data-set">
+                <legend>Gegevens</legend>
+                <label htmlFor="name-input">Naam:
+                    <input
                       type="text"
                       id="name-input"
-                      value={nameValue}
-                      onChange={(e) => setNameValue(e.target.value)}
-                  />
-              </label>
-              <label htmlFor="age-input">Leeftijd:
-                  <input
+                      name="fullName"
+                      value={state.fullName}
+                      onChange={handleChange}
+                    />
+                </label>
+                <label htmlFor="age-input">Leeftijd:
+                    <input
                       type="number"
                       id="age-input"
-                      value={ageValue}
-                      onChange={(e) => setAgeValue(e.target.value)}
-                  />
-              </label>
-          </fieldset>
-          <fieldset className="review-set">
-              <legend>Jouw Review</legend>
-              <label htmlFor="remark-input">Opmerkingen:</label>
-              <textarea
+                      name="age"
+                      value={state.age}
+                      onChange={handleChange}
+                    />
+                </label>
+            </fieldset>
+            <fieldset className="review-set">
+                <legend>Jouw Review</legend>
+                <label htmlFor="remark-input">Opmerkingen:</label>
+                <textarea
                   id="remark-input"
+                  name="review"
                   placeholder="Wat vond je van het recept?"
-                  value={remarkValue}
-                  onChange={(e) => setRemarkValue(e.target.value)}
-              >
-              </textarea>
-              <label htmlFor="newsletter-check">
-                  <input
-                      type="checkbox"
-                      id="newsletter-check"
-                      checked={newsletterChecked}
-                      onClick={() => setNewsletterChecked(!newsletterChecked)}
-                  />
-                  Ik schrijf me in voor de nieuwsbrief
-              </label>
-              <button
-                  type="submit"
-              >
-                  Versturen
-              </button>
-          </fieldset>
-
-
-      </form>
+                  value={state.review}
+                  onChange={handleChange}
+                >
+                </textarea>
+                <label htmlFor="newsletter-check">
+                    <input
+                        type="checkbox"
+                        id="newsletter-check"
+                        checked={newsletterChecked}
+                        onChange={() => setNewsletterChecked(!newsletterChecked)}
+                />
+                    Ik schrijf me in voor de nieuwsbrief
+                </label>
+                <button
+                    type="submit"
+                    disabled={checkValues()}
+                >
+                    Versturen
+                </button>
+            </fieldset>
+        </form>
     </div>
-  );
+    );
 }
 
 export default App;
